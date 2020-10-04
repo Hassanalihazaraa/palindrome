1772035b309a33458822862264f22e91b8a9813e
Hassan <hassanalihazaraa@gmail.com>
2020-10-04 17:16:42 +0200

    initial files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+
+class Palindrome
+{
+    public function checkShortPalindrome(): void
+    {
+
+    }
+}
+
+$solution = new Palindrome;
+$solution->checkShortPalindrome();
